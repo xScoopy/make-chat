555eaaa337ec274c605bd323f15aff0503377ca8
xScoopy <JLeary070@gmail.com>
2021-09-26 21:21:35 -0500

    Users can join and leave chats

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ const server = require('http').Server(app);
 
 //Socket.io
 const io = require('socket.io')(server);
+//We'll store our online users here
+let onlineUsers = {};
 io.on("connection", (socket) => {
-  require('./sockets/chat.js')(io, socket);
+  // Make sure to send the users to our chat file
+  require('./sockets/chat.js')(io, socket, onlineUsers);
 })
 
 const exphbs  = require('express-handlebars');
