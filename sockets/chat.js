38561c9cfb70a644b19724baeda04a20253ba4b4
xScoopy <JLeary070@gmail.com>
2021-09-26 21:27:20 -0500

    messages go to correct channel

--- sockets/chat.js
+++ sockets/chat.js
@@ -10,8 +10,10 @@ module.exports = (io, socket, onlineUsers, channels) => {
   });
 
   socket.on("new message", (data) => {
-    console.log(`🎤 ${data.sender}: ${data.message} 🎤`);
-    io.emit("new message", data);
+    //Save the new message to the channel.
+    channels[data.channel].push({ sender: data.sender, message: data.message });
+    //Emit only to sockets that are in that channel room.
+    io.to(data.channel).emit("new message", data);
   });
   socket.on("get online users", () => {
     //Send over the onlineUsers
@@ -24,17 +26,17 @@ module.exports = (io, socket, onlineUsers, channels) => {
     delete onlineUsers[socket.username];
     io.emit("user has left", onlineUsers);
   });
-  socket.on('new channel', (newChannel) => {
+  socket.on("new channel", (newChannel) => {
     //Save the new channel to our channels object. The array will hold the messages.
     channels[newChannel] = [];
     //Have the socket join the new channel room.
     socket.join(newChannel);
     //Inform all clients of the new channel.
-    io.emit('new channel', newChannel);
+    io.emit("new channel", newChannel);
     //Emit to the client that made the new channel, to change their channel to the one they made.
-    socket.emit('user changed channel', {
-      channel : newChannel,
-      messages : channels[newChannel]
+    socket.emit("user changed channel", {
+      channel: newChannel,
+      messages: channels[newChannel],
     });
   });
 };
